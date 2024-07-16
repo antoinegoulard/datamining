31ee250adbf3ad2a6215f89ea6a7885f578af4d4
Antoine Goulard <antoine.goulard@outlook.com>
2024-07-16 19:32:34 +0200

    First Commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('DATA MINING PROJECT')
+st.subheader('BIA 1 - Group X')
+st.markdown("""### Collaborators :
+- **Mohammad DARMSY LADHA**: mohammad.darmsy-ladha@efrei.net
+- **Ramy ABDELMOULA**: ramy.abdelmoula@efrei.net
+- **Antoine GOULARD**: antoine.goulard@efrei.net
+""")
+
+st.markdown("""---""")
